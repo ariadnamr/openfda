14e1ceefb8b90d3a2e23bb718ef011c3ddb29c49
Ariadna <37300225+ariadnamr@users.noreply.github.com>
2018-03-24 10:39:44 +0100

    Update programa1.py

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -3,6 +3,6 @@ import urllib.request, json
 with urllib.request.urlopen("https://api.fda.gov/drug/label.json") as url:
     data_dictionary = json.loads(url.read().decode())
 
-print("EL identificador del medicamento es:", data["results"][0]["id"])
-print("El propósito del medicamento es:", ((str(data["results"][0]["purpose"])).strip("[]")).strip("'"))
-print("El nombre del fabricante es:", data["results"][0]["openfda"]["manufacturer_name"][0])
+print("EL identificador del medicamento es:", data_dictionary["results"][0]["id"])
+print("El propósito del medicamento es:", ((str(data_dictionary["results"][0]["purpose"])).strip("[]")).strip("'"))
+print("El nombre del fabricante es:", data_dictionary["results"][0]["openfda"]["manufacturer_name"][0])
