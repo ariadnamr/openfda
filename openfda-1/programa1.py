a252c9c75a81cbcc64c5c077e6b5bd5a75c89b42
Ariadna <37300225+ariadnamr@users.noreply.github.com>
2018-03-31 19:11:26 +0200

    Update programa1.py

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -4,5 +4,5 @@ data_dictionary= url.json() #Obtenemos la información procedente de esa url (qu
                             # y le damos un nombre.
 #Mediante busquedas e el diccionario y en las listas que contiene, obtenemos la información que queremos.
 print("El identificador del medicamento es:", data_dictionary["results"][0]["id"])
-print("El propósito del medicamento es:", ((str(data_dictionary["results"][0]["purpose"])).strip("[]")).strip("'"))
+print("El propósito del medicamento es:", data_dictionary["results"][0]["purpose"][0])
 print("El nombre del fabricante es:", data_dictionary["results"][0]["openfda"]["manufacturer_name"][0])
