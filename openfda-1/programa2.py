03728152505ce102297a1d8656695b3579a19eae
Ariadna <37300225+ariadnamr@users.noreply.github.com>
2018-03-31 19:27:20 +0200

    Update programa2.py

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -1,15 +1,18 @@
-import urllib.request, json
+import urllib.request, json #Importamos los módulos necesarios.
 
-with urllib.request.urlopen("https://api.fda.gov/drug/label.json?limit=10") as url:
-    data_dictionary = json.loads(url.read().decode())
+with urllib.request.urlopen("https://api.fda.gov/drug/label.json?limit=10") as url: #Abrimos la url gracias al módulo urllib.
+    data_dictionary = json.loads(url.read().decode()) #Guardamos la información obtenida (en forma de diccionario) en una variable.
 
 
-    for i in range(0,10):
-        print("El identicador del medicamento es:", data_dictionary["results"][i]["id"])
-        if "purpose" in data_dictionary["results"][i]:
-            print("El propósito del medicamento es:", data_dictionary["results"][i]["purpose"][0])
+for i in range(0,10): #Como queremos la información de 10 medicamentos, creamos un bucle for que se repite 10 veces.
+    print("El identicador del medicamento es:", data_dictionary["results"][i]["id"])
+    if "purpose" in data_dictionary["results"][i]: #Como no todos los medicamentos contienen información sobre el propósito o del nombre del fabricante, lo controlamos mediante sentencias condicionales.
+        print("El propósito del medicamento es:", data_dictionary["results"][i]["purpose"][0])
+    else:
+        print("No se encuentra información al respecto. Comprueba si existe el propósito en la página oficial FDA.")
 
-        if "manufacturer_name" in data_dictionary["results"][i]["openfda"]:
-            print("El nombre del fabricante s:", data_dictionary["results"][i]["openfda"]["manufacturer_name"][0])
-        print("")
-        
+    if "manufacturer_name" in data_dictionary["results"][i]["openfda"]:
+        print("El nombre del fabricante s:", data_dictionary["results"][i]["openfda"]["manufacturer_name"][0])
+    else:
+        print("No se encuentra información al respecto. Comprueba si existe el nombre del fabricante en la página oficial FDA.")
+    print("")
