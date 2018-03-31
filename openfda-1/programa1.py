34fa17967e70e5486750dba5091f96dfb9ebaf01
Ariadna <37300225+ariadnamr@users.noreply.github.com>
2018-03-31 19:05:50 +0200

    Update programa1.py

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -1,8 +1,8 @@
-import urllib.request, json
-
-with urllib.request.urlopen("https://api.fda.gov/drug/label.json") as url:
-    data_dictionary = json.loads(url.read().decode())
-
+import requests
+url = requests.get("https://api.fda.gov/drug/label.json") #Nos "conectamos" a la url.
+data_dictionary= url.json() #Obtenemos la información procedente de esa url (que se guarda en forma de diccionario)
+                            # y le damos un nombre.
+#Mediante busquedas e el diccionario y en las listas que contiene, obtenemos la información que queremos.
 print("El identificador del medicamento es:", data_dictionary["results"][0]["id"])
 print("El propósito del medicamento es:", ((str(data_dictionary["results"][0]["purpose"])).strip("[]")).strip("'"))
 print("El nombre del fabricante es:", data_dictionary["results"][0]["openfda"]["manufacturer_name"][0])
