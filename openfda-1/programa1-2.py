3cc229f5db16f36ce3d47a0971ec29c635f19a35
Ariadna <37300225+ariadnamr@users.noreply.github.com>
2018-04-08 16:53:20 +0200

    Update programa1-2.py

--- openfda-1/programa1-2.py
+++ openfda-1/programa1-2.py
@@ -1,8 +1,10 @@
 #Otra forma de realizar el ejercicio(con biblioteca http.client)
-import http.client #Importamos la biblioteca HTTP.client la cual nos permite implementar clientes web de manera muy sencilla.
-import json
 
-headers = {'User-Agent': 'http-client'} #Definimos el tipo de cliente que va a hacer la peticion (integrado en python).
+import json
+import http.client #Importamos la biblioteca HTTP.client la cual nos permite implementar clientes web de manera muy sencilla.
+
+headers = {'User-Agent': 'http-client'} #Definimos el tipo de cliente que va a hacer la peticion 
+                                        #(integrado en python).
 
 conn = http.client.HTTPSConnection("api.fda.gov") #Permite la consexión con el recurso determinado.
 conn.request("GET", "/drug/label.json", None, headers) #Realizamos una petición para obtener el archivo específico.
@@ -17,3 +19,6 @@ repos = json.loads(data_dictionary) #Loads hace que ese json se convierta en str
                                     #en forma de diccionario.
 
 print("El identificador del medicamento es:", repos["results"][0]["id"])
+print("El propósito del medicamento es:", repos["results"][0]["purpose"][0])
+print("El nombre del fabricante es:", repos["results"][0]["openfda"]["manufacturer_name"][0])
+
