c3bb8842b4b7815369431f7b16d673e3f3a41f71
Ariadna <37300225+ariadnamr@users.noreply.github.com>
2018-04-08 17:03:09 +0200

    Update Programa3.py

--- openfda-2/Programa3.py
+++ openfda-2/Programa3.py
@@ -1,18 +1,25 @@
 import requests
-n=0 #Contador mediante el cual controlaremos el argumento "skip".
+n=0 #Contador mediante el cual controlaremos el parámetro "skip".
+
 #Bucle que controla la conexión a las distintas variantes de la url(la cual es controlada por el valor n).
 while True:
     url = requests.get("https://api.fda.gov/drug/label.json?search=active_ingredient:acetylsalicylic&limit=100&skip="+str(n))
     url.close()
 
-    data_dictionary = url.json() #Diccionario que contiene toda la información proveniente de la url cuyo archivo es un json.
+    data_dictionary = url.json() #Diccionario que contiene toda la información proveniente de la url
+                                 #cuyo archivo es un json.
 
-    for medicamento in range(len(data_dictionary["results"])): #Gracias al bucle for, podemos ir imprimiendo cada valor del diccionario que nos interese.
+    #Gracias al bucle for, podemos ir imprimiendo cada valor del diccionario que nos interese.
+    for medicamento in range(len(data_dictionary["results"])):
         if "manufacturer_name" in  data_dictionary["results"][medicamento]["openfda"]:
-            print("El medicamento cuyo identificador es: ", data_dictionary["results"][medicamento]["id"], "ha sido fabricado por: ", data_dictionary["results"][medicamento]["openfda"]["manufacturer_name"][0],"\n")
+            print("El medicamento cuyo identificador es:")
+            print(data_dictionary["results"][medicamento]["id"],"")
+            print("ha sido fabricado por:")
+            print(data_dictionary["results"][medicamento]["openfda"]["manufacturer_name"][0], "\n\n")
         else:
-            print("No se encuentra información sobre el fabricante del medicamento con id:.", data_dictionary["results"][medicamento]["id"], "\n")
-    if len(data_dictionary["results"]) < 100: #En el momento en el la longitud del diccionario sea menor que 100, pararemos el bucle, pues eso significa que ya no hay más medicamentos para analizar.
-        break
-    n= n +100 #Modificación del contador, sumamos 100 para más tarde introducirlo en la url de forma que el parámetro skip se saltará cierta cantidad de medicamentos.
-
+            print("No se encuentra información sobre el fabricante del medicamento con id:")
+            print(data_dictionary["results"][medicamento]["id"],"\n\n")
+    if len(data_dictionary["results"]) < 100: #En el momento en el la longitud del diccionario sea menor que 100,
+        break                                 #pararemos el bucle, pues eso significa que ya no hay más medicamentos para analizar.
+    n= n +100 #Modificación del contador, sumamos 100 para más tarde introducirlo en la url
+              #de forma que el parámetro skip se saltará cierta cantidad de medicamentos.
